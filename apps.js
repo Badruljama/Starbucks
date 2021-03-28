5465a517cfaedddfb9f3f1da1d9649ca750df036
Badruljama <bjjstr8721@gmail.com>
2021-03-27 23:43:52 -0700

    code transfer
    JavaScript coding transfer a apps.js file

--- apps.js
+++ apps.js
@@ -1,1 +1,13 @@
-
+function imgSlider(anything){
+    document.querySelector('.img_1').src = anything;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+function toggleMenu(){
+    let menuToggle = document.querySelector('.toggle');
+    let menu = document.querySelector('.menu-sec');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+};
